10bf16024350552b86398d97a605e9e69f0213ed
Moussa SOW <moussa4.sow@gmail.com>
2021-04-21 01:03:56 +0200

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Moussa SOW on 21/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
