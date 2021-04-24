61dd54e845660162c5760d6b43334dc54f8ac7ef
Moussa SOW <moussa4.sow@gmail.com>
2021-04-24 11:14:33 +0200

    Camera tested

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var picker: UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.allowsEditing = true
     }
 
-
+    @IBAction func takePicture(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            present(picker, animated: true, completion: nil)
+        }else{
+            print("Pas de caméra")
+        }
+        
+    }
+    
+    @IBAction func choosePicture(_ sender: Any) {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //if let photo = info[.originalImage] as? UIImage {
+          //  imageView.image = photo
+            //picker.dismiss(animated: true, completion: nil)
+        //}
+        if let photo = info[.editedImage] as? UIImage {
+            imageView.image = photo
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
 }
 
